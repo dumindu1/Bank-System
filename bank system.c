dddbf1c0167f8dd47441ba3181b2d8961c5c7929
Thilanga Dumindu <90759210+dumindu1@users.noreply.github.com>
2023-12-02 22:31:14 +0530

    Create Bank Management System
    Bank Management System was created.

---
+++ bank system.c
@@ -0,0 +1,5 @@
+#include<stdio.h>// standard Input Output header file 
+#include<stdlib.h>//standard library file
+#include<string.h>//declares the set of functions to work string.
+
+int main_exit;//global variable declararion

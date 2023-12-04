e2ec14663bd2a3a3503d2bb61fea96e1b25f9b98
Thilanga Dumindu <90759210+dumindu1@users.noreply.github.com>
2023-12-04 22:48:43 +0530

    Add Functions for system

--- bank system.c
+++ bank system.c
@@ -3,3 +3,12 @@
 #include<string.h>//declares the set of functions to work string.
 
 int main_exit;//global variable declararion
+
+float interest(float t,float amount,int rate);//interest function declaration
+void newacc();//newacc function declaration
+void update();//update function declaration
+void transact();//transact function declaration
+void checkb();//chechb function declaration
+void delete();//delete function declaration
+void mainmenu();//mainmenu function declaration
+void close();//close function declarion

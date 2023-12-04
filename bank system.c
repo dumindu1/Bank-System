2deb0c4bbabefbccd10c68a594616f592dca35aa
Thilanga Dumindu <90759210+dumindu1@users.noreply.github.com>
2023-12-04 22:50:22 +0530

    create a structure

--- bank system.c
+++ bank system.c
@@ -12,3 +12,25 @@ void checkb();//chechb function declaration
 void delete();//delete function declaration
 void mainmenu();//mainmenu function declaration
 void close();//close function declarion
+
+//create a structure which name is date 
+struct date
+{
+    int month,day,year;
+};
+
+struct{
+
+    //structer members or elements
+    char name[60];
+    int accno,age;
+    char address[60];
+    char citizenship[15];
+    double phoneno;
+    char acctype[10];
+    float amt;
+    struct date dob;
+    struct date deposit;
+    struct date withdraw;
+
+    }add,upd,check,rem,transaction;// declare variable for the structure

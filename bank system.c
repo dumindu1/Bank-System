a754a3b6688b7e73ddd595c1b8334fbeb5c12a39
Thilanga Dumindu <90759210+dumindu1@users.noreply.github.com>
2023-12-06 17:07:19 +0530

    Update main function

--- bank system.c
+++ bank system.c
@@ -34,3 +34,62 @@ struct{
     struct date withdraw;
 
     }add,upd,check,rem,transaction;// declare variable for the structure
+
+    //...........main function definition..............
+    int main()
+{
+  
+    char pass[10],password[10]="pass";//initialization of password="pass"
+    int i;
+ 
+    printf("\n\n\n\t\t\t\t   Bank Management System\n\t\t\t\t\t User Login ");
+    printf("\n\n\n\n\n\t\t\t\tEnter the password to login:");//get password from user(login password is "pass")
+    scanf("%s",pass);//scan the user input
+                     
+    if (strcmp(pass,password)==0)// string comparison
+
+        {
+            printf("\n\nPassword Match!\nLOADING");
+            for(i=0;i<=6;i++) //for loop execution
+             {
+                
+                 printf(". ");
+             }
+                printf("\n");// print the new line
+                system("pause");////terminal wait for a key press
+                system("cls");//clear the terminal
+	   
+            mainmenu();//call the mainmenu function
+        }
+    else
+        {   printf("\n\nWrong password!!\a\a\a");
+
+            login://label 
+            printf("\nEnter 1 to try again and 0 to exit:");//user input
+            scanf("%d",&main_exit);
+            // if user enter the 1 
+            if (main_exit==1)
+                    {
+
+                        system("cls");//clear the terminal
+                        //retry to password
+                        main();//call the mainfunction
+                    }
+            //if user enter the 0
+            else if (main_exit==0)
+                    {
+                    system("cls");
+                    close();//call the close function
+                    }
+            //if user enter a invalid input.(expect the 1 or 0)  
+            else
+                    {
+                    printf("\nInvalid Entered!");
+                    goto login;// go to jump if the user enter a invalid input(expect 0 or 1)
+                    }
+
+        }
+
+
+        return 0;//function will return null function
+}//end the  main function
